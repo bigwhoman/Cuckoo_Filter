f1e11fd1918c389b336cf1bf75be4bd0d97e5a85
bigwhoman <hm.keshvari@gmail.com>
2023-12-26 22:47:49 +0330

    UPDATE : changed the fingerprint and error rate

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.io.File;
-
+import java.util.BitSet;
+import java.util.UUID;
 public class Main {
 
     public static void main(String[] args) {
-        Cuckoo cuckoo = new Cuckoo(8, 4, 3, 8);
+        Cuckoo cuckoo = new Cuckoo(8, 4, 3, 0.05);
         
         cuckoo.insert("hell");
         cuckoo.insert("hool");
@@ -14,8 +15,17 @@ public class Main {
         cuckoo.insert("stell");
 
         System.out.println(cuckoo.lookup("stall"));
+        System.out.println(cuckoo.lookup("sel"));
+        System.out.println(cuckoo.lookup("stell"));
+        System.out.println(cuckoo.lookup("stell"));
+        System.out.println(cuckoo.lookup("stell"));
+        System.out.println(cuckoo.lookup("hell"));
+        System.out.println(cuckoo.lookup("gell"));
+        System.out.println(cuckoo.lookup("bell"));
+        System.out.println(cuckoo.lookup("hill"));
         System.out.println(cuckoo.delete("stall"));
         System.out.println(cuckoo.lookup("stall"));
+        System.out.println(cuckoo.delete("stall"));
         return ;
     }
 
