dc4c8a968192b8313bed6392f85c08a8badc33ca
bigwhoman <hm.keshvari@gmail.com>
2023-12-29 13:12:03 +0330

    added an input output file with test and outputs

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,64 @@
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.File;
-import java.util.BitSet;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.UUID;
-public class Main {
 
+public class Main{
     public static void main(String[] args) {
-        ScalableCuckoo cuckoo = new ScalableCuckoo(2, 4, 500, 0.05,2);
-        cuckoo.insert("hell");
-        cuckoo.insert("hool");
-        cuckoo.insert("hill");
-        cuckoo.insert("still");
-        cuckoo.insert("stall");
-        cuckoo.insert("stell");
+        StringBuilder inputSB = new StringBuilder();
+        StringBuilder outputSB = new StringBuilder();
 
-        System.out.println(cuckoo.lookup("stall")); // true
-        System.out.println(cuckoo.lookup("sel"));   // false
-        System.out.println(cuckoo.lookup("stell")); // true
-        System.out.println(cuckoo.lookup("hool"));  // true
-        System.out.println(cuckoo.lookup("hell"));  // true
-        System.out.println(cuckoo.lookup("gell"));  // false
-        System.out.println(cuckoo.lookup("bell"));  // false
-        System.out.println(cuckoo.lookup("hill"));  // true
-        System.out.println(cuckoo.delete("stall")); // true
-        System.out.println(cuckoo.lookup("stall")); // false
-        System.out.println(cuckoo.delete("stall")); // false
-        return ;
+        Cuckoo cuckoo = new Cuckoo(100, 4, 500, 0.05);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            inputSB.append(command).append("\n");
+            if ("exit".equals(command)){
+                    break;
+            }
+            String[] parts = command.split(" ");
+
+            if (parts.length == 2) {
+                String operation = parts[0];
+                String arg = parts[1];
+
+                if ("insert".equals(operation)) {
+                    boolean result = cuckoo.insert(arg);
+                    System.out.println(result);
+                    outputSB.append(Boolean.toString(result)).append("\n");
+                } else if ("lookup".equals(operation)) {
+                    boolean result = cuckoo.lookup(arg);
+                    System.out.println(result);
+                    outputSB.append(Boolean.toString(result)).append("\n");
+                } else if ("delete".equals(operation)) {
+                    boolean result = cuckoo.delete(arg);
+                    System.out.println(result);
+                    outputSB.append(Boolean.toString(result)).append("\n");
+                } else {
+                    System.out.println("Invalid command!");
+                }
+            } else {
+                System.out.println("Invalid command format!");
+            }
+        }
+
+        BufferedWriter inputWriter = null;
+        BufferedWriter outputWriter = null;
+        try {
+            inputWriter = new BufferedWriter(new FileWriter("inputs.txt"));
+            inputWriter.write(inputSB.toString());
+            inputWriter.close();
+
+            outputWriter = new BufferedWriter(new FileWriter("outputs.txt"));
+            outputWriter.write(outputSB.toString());
+            outputWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
